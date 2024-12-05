47f5eeb8f6f25ccda97967f20dd2d3134f5138ae
kip2 <tqa1no4@gmail.com>
2024-12-05 21:59:19 +0900

    Add clojure.tools.cli

--- scraping/project.clj
+++ scraping/project.clj
@@ -4,6 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
+                 [org.clojure/tools.cli "1.1.230"]
                  [cheshire "5.13.0"]
                  [etaoin "1.1.42"]
                  [lynxeyes/dotenv "1.1.0"]]
